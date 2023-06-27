1434dd08fe63961f4453a9e12cc20da5b1a1c062
npanagiwtidou <npanagiwtidou@gmail.com>
2023-06-27 10:50:32 +0200

    update dockerfile

--- Dockerfile.py
+++ Dockerfile.py
@@ -4,6 +4,7 @@ FROM python:3.8-buster
 #Creating the working directory in Docker
 WORKDIR /app
 
+
 # Copy all the source code into our directory to the Docker image
 COPY . /app
 
@@ -12,4 +13,3 @@ RUN pip install -r requirements.txt
 
 # tell the Docker the command to run inside the container
 CMD ["python","./main.py"]
-
